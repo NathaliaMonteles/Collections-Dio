b566834478b824c753c2270823591fe0b61b7495
Nathalia Monteles <nathalia_martinsm@hotmail.com>
2024-07-01 22:57:34 -0300

    Adicionando o Set

---
+++ src/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,23 @@
+package list.OperacoesBasicas;
+
+import java.lang.reflect.Constructor;
+
+public class Tarefa {
+
+    //atributo
+
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+}
